64d30754659f08091c5d507bd7b82fdc009b46b8
Christopher Werther <wildcatwerther@gmail.com>
2019-09-25 21:29:59 -0700

    stop animation code

--- assets/gif.js
+++ assets/gif.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
+    //Create a array of glfers
 
-    var golfers = [ "Phil Mickelson", "Jordan Spieth", "Brooks Koepka", "Rory McIlroy"];
+    var golfers = [ "Phil Mickelson", "Jordan Spieth", "Brooks Koepka", "Rory McIlroy", "Justin Rose", "Rickie Fowler", "Tiger Woods", "Keegan Bradley"];
 
-    
+    //Create function for gif images
+
     function displayGolfers(){
           
+        //set up link to Giphy
 
         var golfer = $(this).attr("data-person");
+        console.log(golfer); // 'some string'
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         golfer + "&api_key=DAGsNNHsQvcdBxAUXLy0djrLeck5e07P&limit=10"
         
@@ -14,25 +18,35 @@ $(document).ready(function(){
              url: queryURL,
              method: "GET"        
          })
-         
+         //Create the .then function to display the images
+
         .then(function (response){
             var results = response.data;
-            console.log(response.data);
+            console.log(results);
 
-            for (var i = 0; i < golfer.length; i++) {
+            //for loop to dislay the golfers in the array
+            //Pause animation
+
+            for (var i = 0; i < results.length; i++) {
            
                 var gifDiv = $("<div>");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
-                var gifImage = $("<img>");
-                // gifImage.attr("src", still);
-                // gifImage.attr("data-still", still);
-                // gifImage.attr("data-animate"), animated;
-                // gifImage.attr("data-state", "still");
-                // gifImage.addClass("gif-image");
+                var animated = results[i].images.fixed_height.url;
+                var still = results[i].images.fixed_height_still.url;
+                console.log(animated, still);
 
-                gifImage.attr("src", results[i].images.fixed_height.url);
-                
+                var gifImage = $("<img>");
+              
+                gifImage.attr({
+                    "src": animated,
+                    "data-still": still,
+                    "data-animate": animated,
+                    "data-state": "animated",
+                    "class": "gif-image"
+                });
+
+                console.log(gifImage)
 
                 gifDiv.prepend(p);
                 gifDiv.prepend(gifImage);
@@ -40,10 +54,13 @@ $(document).ready(function(){
                 $("#gifImages").prepend(gifDiv);  
                 console.log(gifImage);
             } 
+
+            
         }); 
       
     }  
-    
+        //create the function to add the buttons on the array
+
         function renderButtons() {
             $("#buttons-view").empty();
             
@@ -55,6 +72,7 @@ $(document).ready(function(){
                 $("#buttons-view").append(a);
             }
         }
+        //create function to add golfer buttons
 
         $("#select-golfer").on("click", function(event) {
             event.preventDefault();
@@ -66,20 +84,22 @@ $(document).ready(function(){
         $(document).on("click", ".golfer-btn", displayGolfers);
         renderButtons();
 
+
+
         //Pausing and animating code
 
-        // $(document).on("click", ".gif-image", function() {
-        //     var state = $(this).attr("data-state");                
+        $(document).on("click", ".gif-image", function() {
+            var state = $(this).attr("data-state");                
 
-        //     if (state === "still") {
-        //         $(this).attr("src", $(this).attr("data-animate"));
-        //         $(this).attr("data-state", "animate");
-        //     }
-        //     else {
-        //         $(this).attr("src", $(this).attr("data-still"));
-        //         $(this).attr("data-state", "still");                   
-        //     }  
-        // });
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+            else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");                   
+            }  
+        });
     
     
 
